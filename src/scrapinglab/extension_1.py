b8ae7527eb8847f6fc468f632a98d411fef6ebe7
Maegan Maulder <maegan.maulder@velumlabs.com>
2026-05-18 09:44:50 +0800

    annotate code

--- src/scrapinglab/extension_1.py
+++ src/scrapinglab/extension_1.py
@@ -1,31 +1,42 @@
 import logging
+from logging import Logger
 import asyncio
-from playwright.async_api import async_playwright
-
+from playwright.async_api import (
+    async_playwright,
+    Browser,
+    BrowserContext, 
+    Page, 
+    Locator,
+)
 class Extension1:
-    def __init__(self, browser=None):
-        self.logger = logging.getLogger(self.__class__.__name__)
-        self.browser = browser
-        self.url = 'https://the-internet.herokuapp.com/abtest'
+    def __init__(
+        self,
+        browser: Browser | None = None
+    ) -> None:
+        self.logger: Logger = logging.getLogger(self.__class__.__name__)
+        self.browser: Browser = browser
+        self.url: str = 'https://the-internet.herokuapp.com/abtest'
 
-    async def init_extension(self):
+    async def init_extension(self) -> None:
+        context: None | BrowserContext = None
+        page: None | Page = None
         async with (
             await self.browser.new_context() as context, 
             await context.new_page() as page,
         ):
             await page.goto(self.url)
-            div = page.locator('div.example')
-            header = div.get_by_role('heading', level=3)
+            div: Locator = page.locator('div.example')
+            header: Locator = div.get_by_role('heading', level=3)
 
             await header.wait_for()
 
-            header_text = await header.inner_text()
+            header_text: str = await header.inner_text()
         self._confirm_result(text=header_text)
         await asyncio.sleep(5)
         self.logger.info(f'{self.__class__.__name__} is done...\n')
 
-    def _confirm_result(self, text=''):
-        header_text_list = ['A/B Test Variation 1', 'A/B Test Control']
+    def _confirm_result(self, text: str = '') -> None:
+        header_text_list: list[str] = ['A/B Test Variation 1', 'A/B Test Control']
         if text in header_text_list:
             self.logger.info(f'Current header is "{text}" in "{self.url}"...')
         else:
--- src/scrapinglab/main.py
+++ src/scrapinglab/main.py
@@ -1,9 +1,15 @@
 import logging
+from logging import Logger
 import asyncio
-from playwright.async_api import async_playwright
+from asyncio import TaskGroup
+from playwright.async_api import (
+    async_playwright, 
+    Browser, 
+    Playwright,
+)
 from scrapinglab.extension_1 import Extension1
 
-logger = logging.getLogger(__name__)
+logger: Logger = logging.getLogger(__name__)
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,20 +17,23 @@ logging.basicConfig(
     datefmt="%H:%M:%S"
 )
 
-async def main_init():
+async def main_init() -> None:
+    p: Playwright
+    browser: Browser
     async with (
         async_playwright() as p,
         await p.chromium.launch(headless=False) as browser,
     ):
         ext_list = [Extension1(browser=browser)]
         try:
+            tg: TaskGroup
             async with asyncio.TaskGroup() as tg:
                 for ext in ext_list:
                     tg.create_task(ext.init_extension())
         except ExceptionGroup:
             logger.exception('TaskGroup failed execution:')
 
-def main():
+def main() -> None:
     asyncio.run(main_init())
 
 if __name__ == '__main__':
