ce26ed8196dabf555914689888b006c8c20dac20
Maegan <maeganmaulder420@gmail.com>
2026-06-04 12:11:06 +0800

    Write code for floating_menu.py

--- src/scrapinglab/extensions/floating_menu.py
+++ src/scrapinglab/extensions/floating_menu.py
@@ -2,20 +2,37 @@ from .core import BaseExtension
 from playwright.async_api import (
     Page,
     Locator,
-    FileChooser,
 )
-import asyncio
+
+menu_choice: list[str] = ['Home', 'News', 'Contact', 'About']
 
 class FloatingMenu(BaseExtension):
     url = 'https://the-internet.herokuapp.com/floating_menu'
 
     async def run(self, page: Page) -> None:
         await page.goto(self.url)
-        link_footer: Locator = page.get_by_role(
-            'link', name='Elemental Selenium')
-        self.logger.info('Scrolling to the bottom of the page...')
-        for _ in range(20):
-            await page.mouse.wheel(0, 250)
-            await asyncio.sleep(0.15)
-        # await link_footer.scroll_into_view_if_needed()
-        await page.pause()
+        for i in range(1, 6):
+            menu: Locator = page.locator('div#menu')
+            before: bool = await menu.is_visible()
+
+            await page.mouse.wheel(0, 1000)
+
+            menu: Locator = page.locator('div#menu')
+            after: bool = await menu.is_visible()
+            await self._click_floating_menu(menu=menu)
+
+            if before == after:
+                self.logger.info(
+                    f'Turn {i}: Menu visible after scrolling.')
+            else:
+                self.logger.info(
+                    f'Turn {i}: Menu not visible after scrolling.')
+
+    async def _click_floating_menu(
+        self,
+        menu: Locator,) -> None:
+        for choice in menu_choice:
+            target: Locator = menu.get_by_role('link', name=f'{choice}')
+            await target.click()
+            self.logger.info(target.page.url)
+        
