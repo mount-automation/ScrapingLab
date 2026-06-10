d89224b9b59aa72be9732f629785627bf7376547
Maegan <maeganmaulder420@gmail.com>
2026-06-10 11:31:19 +0800

    Write code for shifting_image.py

--- src/scrapinglab/extensions/shifting_content/shifting_image.py
+++ src/scrapinglab/extensions/shifting_content/shifting_image.py
@@ -1,15 +1,38 @@
-import logging
-from logging import Logger
 from playwright.async_api import (
     Page,
     Locator,
-    Response,
+    FloatRect,
 )
 from .shifting_page import ShiftingPage
 
 class ShiftingImage(ShiftingPage):
     async def proceed(self) -> None:
         page: Page = await self.init_page()
-        link_list = await page.get_by_role('link', name='click here').all()
+        link_list: list[Locator] = await page.get_by_role(
+            'link', name='click here').all()
         for link in link_list:
-            self.logger.info(f'{await link.get_attribute('src')}')
+            mode_url: str|None = await link.get_attribute('href')
+            if not mode_url:
+                raise ValueError('Mode url not found.')
+            if 'mode=random' in mode_url and 'pixel_shift' in mode_url:
+                await page.locator('img.shift').wait_for(state='visible')
+                image_scale_before: FloatRect|None = await page.locator(
+                    'img.shift').bounding_box()
+                await link.click()
+                await page.wait_for_load_state('domcontentloaded')
+                image_scale_after: FloatRect|None = await page.locator(
+                    'img.shift').bounding_box()
+                if image_scale_before and image_scale_after:
+                    status = (
+                        image_scale_before['y'] == image_scale_after['y']
+                        and 
+                        image_scale_before['x'] != image_scale_after['x']
+                    )
+                    if status:
+                        self.logger.info(
+                            'Image successfully captured '
+                            'and its position shifted.')
+                    else:
+                        self.logger.info('Something went wrong during the image scale checking.')
+                        self.logger.info(f'{image_scale_before}')
+                        self.logger.info(f'{image_scale_after}')
