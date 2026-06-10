0a1daeaa8174259ea069bad10c495c34c74833a2
Maegan <maeganmaulder420@gmail.com>
2026-06-10 21:56:23 +0800

    Implement minor fixes in tables.py

--- src/scrapinglab/extensions/tables.py
+++ src/scrapinglab/extensions/tables.py
@@ -16,7 +16,7 @@ class Tables(BaseExtension):
         headers: list[str] = await self._parse_header(table=table_1)
         records: list[dict] = await self._parse_body(table=table_1, headers=headers)
         for record in records: 
-            print(record)
+            self.logger.info(record)
 
     async def _parse_body(self, table: Locator, headers: list[str]) -> list[dict]:
         body_tr_list: list[Locator] = await table.locator(
@@ -32,10 +32,6 @@ class Tables(BaseExtension):
             full_record.append(record)
         return full_record
              
-
-        
-
-
     async def _parse_header(self, table: Locator) -> list[str]:
         head_tr: Locator = table.locator('thead').locator('tr')
         head_th_list: list[Locator] = await head_tr.locator(
