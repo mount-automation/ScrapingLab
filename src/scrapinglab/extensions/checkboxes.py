7d636ec967419374843f2cce94844163ba1ebb1c
mount-automation <125748420+mount-automation@users.noreply.github.com>
2026-06-17 14:43:17 +0800

    Refactor code in checkboxes.py

--- src/scrapinglab/extensions/checkboxes.py
+++ src/scrapinglab/extensions/checkboxes.py
@@ -1,31 +1,19 @@
-import logging
 import asyncio
-from logging import Logger
 from playwright.async_api import (
     Page,
-    Browser,
     Locator,
-    BrowserContext
 )
+from .core import BaseExtension
 
-class CheckBoxes:
-    def __init__(self, browser: Browser|None = None) -> None:
-        self.browser: Browser = browser
-        self.url: str = 'https://the-internet.herokuapp.com/checkboxes'        
-        self.logger: Logger = logging.getLogger(self.__class__.__name__)
+class CheckBoxes(BaseExtension):
+    url = 'https://the-internet.herokuapp.com/checkboxes' 
     
-    async def init_extension(self) -> None:
-        page: Page
-        context: BrowserContext
-        async with (
-            await self.browser.new_context() as context,
-            await context.new_page() as page,
-        ):
-            await page.goto(self.url)
-            if await self._click_checkbox(page=page):
-                self.logger.info('Checkbox stage done...')
+    async def run(self, page: Page) -> None:
+        await page.goto(self.url)
+        if await self._click_checkbox(page=page):
+            self.logger.info('Checkbox stage done...')
 
-    async def _click_checkbox(self, page: Page|None = None) -> bool:
+    async def _click_checkbox(self, page: Page) -> bool:
         checkbox_list: list[Locator] = await page.get_by_role(
             'checkbox').all()
         for id, checkbox in enumerate(checkbox_list, start=1):
