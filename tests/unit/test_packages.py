6351e6f7b33f11f6dcd315d65e195313a9b82ddd
mount-automation <125748420+mount-automation@users.noreply.github.com>
2026-06-18 16:58:07 +0800

    Write test_packages.py

--- tests/unit/test_packages.py
+++ tests/unit/test_packages.py
@@ -2,6 +2,7 @@ import pkgutil
 import scrapinglab
 import importlib.util
 from pathlib import Path
+from conftest import assert_subpackage_have_init
 
 def test_package_has_init_file():
     scrapinglab_path_raw = scrapinglab.__path__
@@ -11,16 +12,4 @@ def test_package_has_init_file():
     scrapinglab_path = Path(scrapinglab_path_raw[0])
     for module_info in pkgutil.walk_packages(
         [scrapinglab_path], 'scrapinglab.'):
-        if module_info.ispkg:
-            module_spec = importlib.util.find_spec(module_info.name)
-            assert module_spec, (
-                f'Module specifications could not be retrieved.')
-            assert module_spec.origin, (
-                f'Module specification path could not be retrieved. '
-                f'This might due to this package not having an '
-                f'__init__.py file. Package in question is: '
-                f'{module_info.name}')
-            package_path = Path(module_spec.origin)
-            assert '__init__' in module_spec.origin, (
-                f'The following package has no __init__.py file: '
-                f'{package_path.stem}')
+        assert_subpackage_have_init(module_info=module_info)
