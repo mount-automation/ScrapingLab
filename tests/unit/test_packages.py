7a2035085b741a33252cfc1b954ed28f4b6393a9
mount-automation <125748420+mount-automation@users.noreply.github.com>
2026-06-20 14:30:06 +0800

    Write code in test_packages.py

--- tests/unit/test_packages.py
+++ tests/unit/test_packages.py
@@ -1,8 +1,7 @@
 import pkgutil
 import scrapinglab
-import importlib.util
 from pathlib import Path
-from conftest import assert_subpackage_have_init
+from helper import asserter
 
 def test_package_has_init_file():
     scrapinglab_path_raw = scrapinglab.__path__
@@ -12,4 +11,4 @@ def test_package_has_init_file():
     scrapinglab_path = Path(scrapinglab_path_raw[0])
     for module_info in pkgutil.walk_packages(
         [scrapinglab_path], 'scrapinglab.'):
-        assert_subpackage_have_init(module_info=module_info)
+        asserter.assert_subpackage_have_init(module_info=module_info)
