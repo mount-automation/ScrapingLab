24e0eaa6fc1ca352853c94d36c587f603864790f
mount-automation <125748420+mount-automation@users.noreply.github.com>
2026-06-21 15:23:54 +0800

    Change code structure.

--- tests/unit/test_extensions.py
+++ tests/unit/test_extensions.py
@@ -2,14 +2,9 @@ from helper import (
     asserter,
     provider,)
 import pytest
-from pkgutil import ModuleInfo
-import inspect
-import importlib
 import validators
-from pathlib import Path
-import scrapinglab.extensions as extensions
 from scrapinglab.extensions.core import BaseExtension
-from urllib.parse import urlparse
+from helper.provider import LoadedExtensionMainClassCase
 
 @pytest.mark.parametrize(
     'module_info',
@@ -19,51 +14,34 @@ def test_extensions_package_have_init_file(module_info):
     asserter.assert_subpackage_have_init(module_info=module_info)
 
 @pytest.mark.parametrize(
-    'loaded_module, loaded_module_main_cls',
+    'case',
     provider.get_extension_main_class(),
     ids=provider.get_extension_class_ids(),)
 class TestExtensions:
     def test_extensions_have_url(
         self,
-        loaded_module, 
-        loaded_module_main_cls
+        case: LoadedExtensionMainClassCase,
     ):
-        assert 'url' in loaded_module_main_cls.__dict__, (
-            f'Variable url not found in: {
-                loaded_module_main_cls.__module__}')
-        assert validators.url(
-            loaded_module_main_cls.__dict__['url']
-            ) is True, (
-            f'url string is not valid: {
-                loaded_module_main_cls.__dict__["url"]
-            }')
+        assert 'url' in case.cls.__dict__, (f'Variable url not found in: '
+            f'{case.cls.__module__}'
+        )
+        assert validators.url(case.cls.__dict__['url']) is True, (
+            f'url string is not valid: {case.cls.__dict__["url"]}')
 
-    # def test_extensions_have_run_method(self, module_info: ModuleInfo):
-    #     module = importlib.import_module(module_info.name)
-    #     classes = inspect.getmembers(module, inspect.isclass)
-    #     for _, cls in classes:
-    #         check = (
-    #             cls.__module__.startswith(module.__name__)
-    #             and
-    #             cls is not BaseExtension
-    #         )
-    #         if check:
-    #             assert 'run' in cls.__dict__, (
-    #                 f'Object "run" not found in {cls.__module__}')
-    #             assert callable(cls.__dict__['run']), (
-    #                 f'Object "run" is not a function.')
+    def test_extensions_have_run_method(
+        self,
+        case: LoadedExtensionMainClassCase,
+    ):
+        assert 'run' in case.cls.__dict__, (
+            f'Object "run" not found in '
+            f'{case.cls.__module__}')
+        assert callable(case.cls.__dict__['run']), (
+            f'Object "run" is not a function.')
 
-    # def test_extension_inherit_from_base_extension(
-    #     self, module_info: ModuleInfo):
-    #     module = importlib.import_module(module_info.name)
-    #     classes = inspect.getmembers(module, inspect.isclass)
-    #     for _, cls in classes:
-    #         check = (
-    #             cls.__module__.startswith(module.__name__)
-    #             and
-    #             cls is not BaseExtension
-    #         )
-    #         if check:
-    #             assert issubclass(cls, BaseExtension), (
-    #                 f'Extension does not inherit from ' 
-    #                 f'BaseExtension class: {cls.__module__}')
+    def test_extension_inherit_from_base_extension(
+        self, 
+        case: LoadedExtensionMainClassCase,
+    ):
+        assert issubclass(case.cls, BaseExtension), (
+            f'Extension does not inherit from ' 
+            f'BaseExtension class: {case.cls.__module__}')
