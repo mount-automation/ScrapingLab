f1c25853360fe678f4be82d6d1f4841efa62a6a6
mount-automation <125748420+mount-automation@users.noreply.github.com>
2026-06-21 09:50:05 +0800

    Compartmentalize logics based on scope for better handling and clarity.

--- tests/unit/helper/provider.py
+++ tests/unit/helper/provider.py
@@ -5,6 +5,7 @@ from pkgutil import ModuleInfo
 from pathlib import Path
 import scrapinglab.extensions as extensions
 from scrapinglab.extensions import BaseExtension
+from types import ModuleType
 
 def get_all_extensions():
     assert len(extensions.__path__) == 1, (
@@ -18,27 +19,36 @@ def get_all_extensions():
         module_info_list.append(module_info)
     return module_info_list
 
-def get_formatted_ids():
+def get_extension_module_ids():
     module_info_list: list[ModuleInfo] = get_all_extensions()
     formatted_ids = []
     for module_info in module_info_list:
         formatted_ids.append(module_info.name)
     return formatted_ids
 
-from types import ModuleType
-LOADED_MODULE_CLASSES = list[tuple[str, type]]
-LOADED_EXTENSION_ARTIFACTS = tuple[ModuleType, LOADED_MODULE_CLASSES]
-def get_extension_classes(
+def get_extension_main_class():
     module_info: ModuleInfo
-) -> LOADED_EXTENSION_ARTIFACTS:
-    module: ModuleType = importlib.import_module(module_info.name)
-    classes: LOADED_MODULE_CLASSES = inspect.getmembers(
-        module, inspect.isclass)
-    return module, classes
+    extension_main_class_list = []
+    for module_info in get_all_extensions():
+        loaded_module = importlib.import_module(module_info.name)
+        loaded_module_cls_list = inspect.getmembers(
+            loaded_module, inspect.isclass)
+        for _, cls in loaded_module_cls_list:
+            if is_local_main_class(cls=cls, module=loaded_module):
+                extension_main_class_list.append((loaded_module, cls))
+    return extension_main_class_list
 
-def check_loaded_module_main_class(cls: type, loaded_module: ModuleType):
+def get_extension_class_ids():
+    module: ModuleType
+    cls: type
+    extension_class_ids = []
+    for module, cls in get_extension_main_class():
+        extension_class_ids.append(f'{module.__name__}.{cls.__name__}')
+    return extension_class_ids
+
+def is_local_main_class(cls: type, module: ModuleType):
     return (
-        cls.__module__.startswith(loaded_module.__name__) 
+        cls.__module__.startswith(module.__name__)
         and
         cls is not BaseExtension
     )
